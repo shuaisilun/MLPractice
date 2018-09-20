f3ec51ada9cfe8bb085ea4b6208048702f8c4ecb
SilunShuai <534693199@qq.com>
2018-09-20 16:46:59 +0800

    Add regression Tree

---
+++ regreTree.py
@@ -0,0 +1,10 @@
+from numpy import  *
+
+def loadDataSet(fileName):      #general function to parse tab -delimited floats
+    dataMat = []                #assume last column is target value
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float,curLine) #map all elements to float()
+        dataMat.append(fltLine)
+    return dataMat
